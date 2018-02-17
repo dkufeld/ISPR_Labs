d56cbe8c23dada68c65fbbdf4aca2a603a0cc880
Demian Kufeld <dn.kufeld@alumnos.upm.es>
2018-02-17 14:28:52 +0100

    added lab1

---
+++ lab1/lab1.runs/synth_1/.Xil/system_wrapper_propImpl.xdc
@@ -0,0 +1,3 @@
+set_property SRC_FILE_INFO {cfile:{C:/Users/fpga/Desktop/ISPR_Demian/vivadoLab1/lab1/lab1.srcs/constrs_1/imports/Lab 1 sources-20180207/lab1_zybo.xdc} rfile:{../../../lab1.srcs/constrs_1/imports/Lab 1 sources-20180207/lab1_zybo.xdc} id:1} [current_design]
+set_property src_info {type:XDC file:1 line:4 export:INPUT save:INPUT read:READ} [current_design]
+set_property PACKAGE_PIN Y16 [get_ports btn_tri_io]
---
+++ lab1/lab1.sdk/lab1/Debug/src/lab1_1.d
@@ -0,0 +1,49 @@
+src/lab1_1.o src/lab1_1.o: ../src/lab1_1.c \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xgpiops.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xgpiops_hw.h \
+ ../../lab1_bsp/ps7_cortexa9_0/include/xplatform_info.h
+
+../../lab1_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xgpiops.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xgpiops_hw.h:
+
+../../lab1_bsp/ps7_cortexa9_0/include/xplatform_info.h:
